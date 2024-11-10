9d42db6382e8c687291ebd0e4c1a20a65858af3c
Celal Karakoc <c.karakoc@live.nl>
2024-11-10 19:35:57 +0100

    Added 404 page; Added initial Homepage.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { QRCodeComponent } from './qrcode/qrcode.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', component: QRCodeComponent, pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'qrcode', component: QRCodeComponent },
-  // { path: '404', component: PageNotFoundComponent },
-  // { path: '**', redirectTo: '/404', pathMatch: 'full' },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
