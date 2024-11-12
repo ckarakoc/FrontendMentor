7588ba10ef50861951bdba656b7678a371622af6
Celal Karakoc <c.karakoc@live.nl>
2024-11-12 13:09:45 +0100

    Initial blog-preview-card component; Will work on home page for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { QRCodeComponent } from './qrcode/qrcode.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { BlogPreviewCardComponent } from './blog-preview-card/blog-preview-card.component';
 
 export const routes: Routes = [
-  { path: '', component: QRCodeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'blog-preview', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'qrcode', component: QRCodeComponent },
+  { path: 'blog-preview', component: BlogPreviewCardComponent },
   { path: '404', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
