2dcd8e8f569a739c1c261b87f12a5f9c90efa59c
Celal Karakoc <c.karakoc@live.nl>
2024-11-28 14:55:38 +0100

    Added skeleton four card feature.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { CircularMenuComponent } from './circular-menu/circular-menu.component';
 import { SocialLinksComponent } from './social-links/social-links.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { ProductPreviewCardComponent } from './product-preview-card/product-preview-card.component';
+import { FourCardFeatureComponent } from './four-card-feature/four-card-feature.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -18,5 +19,6 @@ export const routes: Routes = [
   { path: 'test', component: CircularMenuComponent },
   { path: 'recipe', component: RecipeComponent },
   { path: 'product-preview', component: ProductPreviewCardComponent },
+  { path: 'four-card', component: FourCardFeatureComponent },
   { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
