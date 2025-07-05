2732d12f0566ae97e5a44eaf7978b8e919061522
Celal Karakoç <c.karakoc@live.nl>
2025-07-05 11:29:35 +0200

    Fixed several design flaws with the home-cards. Added several todos.

--- src/app/home-card/home-card.component.ts
+++ src/app/home-card/home-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home-card',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgStyle
+  ],
   templateUrl: './home-card.component.html',
   styleUrl: './home-card.component.css'
 })
 export class HomeCardComponent {
+  cardTitle = input<string>('Card Title');
+  imgUrl = input.required<string>();
+  cardContent = input<string>('Card content goes here.');
 
+  toggleImgModal() {
+
+  }
 }
